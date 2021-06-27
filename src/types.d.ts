77c047f58eeabe3cb434838b89e4c13b986951df
itstudent17 <itstudent17@outlook.com>
2021-06-27 19:42:14 +0300

    Added User type

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+type Geo = {
+  lat: number;
+  lng: number;
+};
+
+type Adress = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Adress;
+  phone: string;
+  website: string;
+  company: Company;
+};
