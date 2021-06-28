5a5ae86bfd8ff6f30e2fe396442dc1a584be1411
itstudent17 <itstudent17@outlook.com>
2021-06-28 17:09:55 +0300

    New App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import "./App.css";
+import { useEffect } from "react";
+import store from "./store/store";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 import CardList from "./components/CardList";
+import { observer } from "mobx-react-lite";
+import Message from "./components/Message";
+import { baseUrl } from "./constants";
 
-const App = () => {
+const App: React.FC = observer(() => {
+  useEffect(() => {
+    store.fetchUsers(baseUrl);
+  }, []);
+
   return (
     <div className="container">
-      <CardList />
+      <Header />
+      <SearchBar />
+      {store.state === "pending" && <Message text={"Still loading..."} />}
+      {store.state === "error" && (
+        <Message text={"Error happened. Try to reload the page..."} />
+      )}
+      {store.state === "done" ? (
+        store?.users?.length ? (
+          <CardList />
+        ) : (
+          <Message
+            text={"There are no matches. Try to change the text for searching"}
+          />
+        )
+      ) : null}
     </div>
   );
-};
+});
 
 export default App;
